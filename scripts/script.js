403d941842239dd6e633fb10fa745af4ea562c4a
Armin <arminsuraj@gmail.com>
2021-05-17 18:49:01 -0700

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -6,13 +6,50 @@ const setState = router.setState;
 // Make sure you register your service worker here too
 
 document.addEventListener('DOMContentLoaded', () => {
+  let entryCount = 1;
+
   fetch('https://cse110lab6.herokuapp.com/entries')
     .then(response => response.json())
     .then(entries => {
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
+        newPost.num = entryCount;
+        newPost.id = 'entry ' + String(entryCount);
+
+        newPost.addEventListener('click',() => {
+          setState({location: 'entry', entryNum: newPost.num});
+        });
+
         document.querySelector('main').appendChild(newPost);
+        entryCount++;
       });
     });
+  registerSW();
 });
+ 
+document.querySelector('header img').addEventListener('click', () => {
+  setState({location: 'settings'});
+});
+
+document.querySelector('header h1').addEventListener('click', () => {
+    setState({location: 'homepage'});
+});
+
+window.addEventListener('popstate', (event) => {
+  setState(event.state);
+});
+
+const registerSW = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', (event) => {
+        navigator.serviceWorker.register('./../sw.js')
+          .then((registration) => {
+            console.log('sw registration complete: ' + registration.scope);
+          },
+          (err) => {
+            console.error('sw registration failed: ' + err);
+          });
+    });
+  }
+}
--- sw.js
+++ sw.js
@@ -1,6 +1,45 @@
 // sw.js - Service Worker
+// Code from https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 
-// You will need 3 event listeners:
-//   - One for installation
-//   - One for activation ( check out MDN's clients.claim() for this step )
-//   - One for fetch requests
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('v1').then((cache) => {
+        return cache.addAll([
+          './',
+          './index.html',
+          './style.css',
+          './scripts/script.js',
+          './scripts/router.js',
+          './components/entry-page.js',
+          './components/journal-entry.js',
+        ]);
+      })
+    );
+  });
+
+self.addEventListener('fetch', (event) => {
+event.respondWith(
+    caches.match(event.request).then((resp) => {
+    return resp || fetch(event.request).then((response) => {
+        return caches.open('v1').then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+        });
+    });
+    })
+);
+});
+
+self.addEventListener('activate', (event) => {
+var cacheKeeplist = ['v2'];
+
+event.waitUntil(
+    caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+        if (cacheKeeplist.indexOf(key) === -1) {
+        return caches.delete(key);
+        }
+    }));
+    })
+);
+});
